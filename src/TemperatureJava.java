047ab733435976a661848fc5f5d3566277e8279e
Shadows003 <Shadows003@Shadows>
2016-03-16 23:13:26 +0700

    new commit

--- src/TemperatureJava.java
+++ src/TemperatureJava.java
@@ -23,7 +23,7 @@ public class TemperatureJava {
             System.out.println("Temperature Converter");
             System.out.println("4. Dari Kelvin");
             System.out.println("4. Dari Kelvin");
-            System.out.println("4. Dari BlackD3vil5");
+            System.out.println("4. Dari BkD3vil5");
             System.out.println("4. Dari Ray4");
             Scanner pilih = new Scanner(System.in);
             System.out.println("Pilih :");
